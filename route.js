7d95e8e197eef1f1547068457526a45f0e64c992
TiagoSilva-FSD <tiago.shonen@gmail.com>
2022-11-14 10:45:02 -0300

    backend pizzaria

--- route.js
+++ route.js
@@ -11,7 +11,6 @@ const database = [];
 // Rota raiz 
 router.get('/', (req, res) => {
     res.send({
-                "Title": "Projeto Tera - Plataforma de Ensino - Backend",
                 "Module": "NodeJs, Express, AWS Amazon",
                 "Author": "Tiago Silva",
                 "Version": "0.8.0",
@@ -21,43 +20,44 @@ router.get('/', (req, res) => {
 
 // Cadastrar
 router.post('/creat', (req, res) => {
-    let user = req.body;
-    database.push(user);
-    res.status(201).send(user);
-})
-
-
-// Login
-router.post('/login', (req, res) => {
-    let userEmail = req.body.email;
-    let userPswd = req.body.password;
-    for (let user of database) {
-        if (user['email'] == userEmail && user['password'] == userPswd) {
-            res.status(200).send(user);
-        } else {
-            res.sendStatus(204);
-        }
+    let secret = "djcfRdyB34";
+    let key = req.body.auth;
+    if (key === secret && database.length < 20) {
+        let dados = req.body;
+        dados['auth'] = '';
+        database.push(dados);
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(403);
     }
 })
 
 
-// Atualizar
-router.put('/update/:id', (req, res) => {
-    let userId = req.params.id;
-    let user = database[userId];
-    user["name"] = req.body.name;
-    user["email"] = req.body.email;
-    user["password"] = req.body.password;
-    database[userId] = user;
-    res.status(200).send(user);
+// Listar
+router.post('/doc', (req, res) => {
+    let secret = "djcfRdyB34";
+    let key = req.body.auth;
+    if (key === secret) {
+        let data = JSON.stringify(database);
+        res.status(200).send(data);
+    } else {
+        res.status(403);
+    }
 })
 
 
 // Deletar
-router.delete('/user/:id', (req, res) => {
-    let id = req.params.id;
-    database[id] = {};
-    res.status(204).send(database[id]);
+router.post('/del', (req, res) => {
+    let secret = "djcfRdyB34";
+    let key = req.body.auth;
+    if (key === secret) {
+        while(database.length > 0){
+            database.pop();
+        }
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(403);
+    }
 })
 
 
